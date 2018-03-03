4c3ac2dbeca6b57285c95ed478c1596947ac21b1
agus muhajir <hajirodeon@gmail.com>
2018-03-03 16:55:04 +0700

    first commit

---
+++ platforms/android/platform_www/cordova_plugins.js
@@ -0,0 +1,29 @@
+cordova.define('cordova/plugin_list', function(require, exports, module) {
+module.exports = [
+    {
+        "id": "cordova-plugin-device.device",
+        "file": "plugins/cordova-plugin-device/www/device.js",
+        "pluginId": "cordova-plugin-device",
+        "clobbers": [
+            "device"
+        ]
+    },
+    {
+        "id": "cordova-plugin-inappbrowser.inappbrowser",
+        "file": "plugins/cordova-plugin-inappbrowser/www/inappbrowser.js",
+        "pluginId": "cordova-plugin-inappbrowser",
+        "clobbers": [
+            "cordova.InAppBrowser.open",
+            "window.open"
+        ]
+    }
+];
+module.exports.metadata = 
+// TOP OF METADATA
+{
+    "cordova-plugin-device": "2.0.1",
+    "cordova-plugin-inappbrowser": "2.0.2",
+    "cordova-plugin-whitelist": "1.3.3"
+};
+// BOTTOM OF METADATA
+});
